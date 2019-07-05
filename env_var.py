afae63fc2f75cf627a0f8fceee25949edf5dcea4
Priyanga G <priyanga.g@hpe.com>
2019-07-05 13:26:43 +0530

    Update env_var.py

--- env_var.py
+++ env_var.py
@@ -3,4 +3,7 @@ f=open("env_var.txt","a+")
 
 for item, value in os.environ.items():
     f.write('{}={}'.format(item, value))
-os.stat("env_var.txt").st_size == 0
+if os.stat("env_var.txt").st_size == 0:
+    print("Hai")
+else:
+    print("bye")
