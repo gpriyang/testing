618dbd8e0bd5efe1d296c2b16988854b5016ede1
Priyanga G <priyanga.g@hpe.com>
2019-07-05 13:31:53 +0530

    Update env_var.py

--- env_var.py
+++ env_var.py
@@ -1,8 +1,8 @@
 import os
-f=open("env_var.txt","a+")
-
+f=open("env_var.txt","a")
 for item, value in os.environ.items():
     f.write('{}={}'.format(item, value))
+f.close()
 if os.stat("env_var.txt").st_size == 0:
     print("Hai")
 else:
