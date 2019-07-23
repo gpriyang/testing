5541ee3837f773629a8fb822459769cfb4eb0f13
Priyanga G <priyanga.g@hpe.com>
2019-07-23 11:49:40 +0530

    Delete env_var.py

--- env_var.py
+++
@@ -1,6 +0,0 @@
-import os
-with open("env_var.txt", 'w') as f:
-    for item, value in os.environ.items():
-        if item.startswith("collie_"):
-            var1=item.replace("collie_","") +"="+ value
-            f.write("%s\n" % var1)
