6d86f3e0b0e76f2b3e75b0c2e1b157b65182422c
Priyanga G <priyanga.g@hpe.com>
2019-07-26 13:26:07 +0530

    Update big_ole_test.sh

--- bin/big_ole_test.sh
+++ bin/big_ole_test.sh
@@ -8,4 +8,5 @@ echo "Run the testing OAUTH BLAZEMETER TEST"
 
 pip install --upgrade pip wheel
 pip install bzt
-bzt blazemeter/testing_auth.yml -o modules.blazemeter.report-name="CircleCI Build ${CIRCLE_BUILD_NUM}" -o modules.blazemeter.token=${BLAZZE_API_KEY}
+bzt blazemeter/testing_auth.yml -o modules.blazemeter.report-name="CircleCI Build Jmeter ${CIRCLE_BUILD_NUM}" -o modules.blazemeter.token=${BLAZZE_API_KEY}
+bzt blazemeter/testing_blazem_demo.yml -o modules.blazemeter.report-name="CircleCI Build blaze demo ${CIRCLE_BUILD_NUM}" -o modules.blazemeter.token=${BLAZZE_API_KEY}
